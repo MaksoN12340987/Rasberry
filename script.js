da9e0aebc8b4171a7ca376312fe9e4de9b47c0f6
Maxim Andreev <andrianov_maksim@outlook.com>
2024-01-09 13:36:32 +0300

    restructed repo

---
+++ script.js
@@ -0,0 +1,13 @@
+"use script";
+
+function Restart2600() {
+    let status = "";
+    let enterStatus = 0;
+    if (enterStatus != 0) {
+        status = "OK"
+    } 
+    else {
+        status = "ERROR"
+    }
+    return status;
+} 
